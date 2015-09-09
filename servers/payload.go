941713db08ff24bd66e3f84340dfd4ba1640a1ae
ChrisMcKenzie <chris@kbsurfer.com>
2015-09-09 12:51:03 -0700

    forgot to support username and password

--- servers/payload.go
+++ servers/payload.go
@@ -4,7 +4,12 @@ func GetServersFromPayload(opt map[string]interface{}) (servers []Server, err er
 	var list []Server
 	// TODO(ChrisMcKenzie): handle nil server list
 	for _, val := range opt["list"].([]interface{}) {
-		list = append(list, Server{Address: val.(string)})
+		server := val.(map[string]string)
+		list = append(list, Server{
+			Address:  server["address"],
+			User:     server["username"],
+			Password: server["password"],
+		})
 	}
 
 	servers = list
