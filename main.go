2cce4fb2ad18fc39a390d070932b22730935b03e
ChrisMcKenzie <chris@kbsurfer.com>
2015-10-15 15:55:43 -0700

    removed everything to prepare for new architecture

--- main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/ChrisMcKenzie/dropship/commands"
-
-func main() {
-	commands.Execute()
-}
--- plugin/servers/servers.go
+++
@@ -1,9 +0,0 @@
-package servers
-
-type (
-	Server struct {
-		Address  string `json:"address"`
-		User     string `json:"username"`
-		Password string `json:"password"`
-	}
-)
