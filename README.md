a9fe2bde656cbf6310b0af51944dac4a93dba37a
ChrisMcKenzie <chris@kbsurfer.com>
2015-10-22 15:48:27 -0700

    better packaging and a README

---
+++ README.md
@@ -0,0 +1,18 @@
+# Dropship
+
+Dropship is a simple tool for get software in the form of artficats on a cdn
+installed on to a server automatically.
+
+## Features
+
+- Automatically performs md5sum checks of artifact that is on server and remote
+and will download automatically
+- Distributed sequential updates
+- Multiple Artifact Repository Support
+
+## Roadmap
+
+- [ ] Support for Amazon S3, and FTP
+- [ ] Support for different file types _(currently only tar.gz)_
+- [ ] Reporting system
+- [ ] Redis, etcd for semaphore
--- package.sh
+++ package.sh
@@ -1,17 +1,21 @@
-GOOS=linux
-GOARCH=amd64
 
 mkdir -p packaging/output
 
 set -x
-go build -o packaging/root/usr/local/bin/dropship main.go
+GOARCH=amd64 GOOS=linux go build -o packaging/root/usr/local/bin/dropship main.go
 
-fpm -s dir -t deb -n "dropship" -a amd64 -v $1 \
-  -p packaging/output/dropship.deb \
-  -m "Chris McKenzie <chris@chrismckenzie.io>" \
-  --description "Dropship code deployment agent"
+fpm -s dir -t deb -n dropship -v "$1" -p packaging/output/dropship.deb \
+  --deb-priority optional --category admin \
   --force \
+  --after-install packaging/scripts/postinstall.deb \
+  --before-remove packaging/scripts/prerm.deb \
+  --after-remove packaging/scripts/postrm.deb \
   --deb-compression bzip2 \
+  --url https://github.com/chrismckenzie/dropship \
+  --description "Dropship automatically keeps you software up to date" \
+  -m "Chris McKenzie <chris@chrismckenzie.io>" \
+  -a amd64 \
+  --config-files /etc/dropship.d/dropship.hcl \
   packaging/root/=/
 
 cp packaging/output/dropship.deb .
